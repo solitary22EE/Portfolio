57769baa64ca6676873a6e85e4e3e3348b7dc7a9
solitary22EE <rajkumarkarikkal8942@gmail.com>
2026-02-18 06:58:36 +0530

    Testing tailwind working or not

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,29 @@ import About from "./components/About";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
-import "./App.css";
+// import "./App.css";
+import "./index.css";
 
-function App() {
+// function App() {
+//   return (
+//     <>
+//       <Navbar />
+//       <Home />
+//       <About />
+//       <Skills />
+//       <Projects />
+//       <Contact />
+//     </>
+//   );
+// }
+
+// export default App;
+export default function App() {
   return (
-    <>
-      <Navbar />
-      <Home />
-      <About />
-      <Skills />
-      <Projects />
-      <Contact />
-    </>
-  );
+    <div className="h-screen flex items-center justify-center bg-black">
+      <h1 className="text-5xl font-bold text-red-500">
+        Tailwind Working ✅
+      </h1>
+    </div>
+  )
 }
-
-export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,37 @@
-import React from "react";
+import { useState, useEffect } from "react";
 
 function Navbar() {
+  const [dark, setDark] = useState(false);
+
+  useEffect(() => {
+    if (dark) document.documentElement.classList.add("dark");
+    else document.documentElement.classList.remove("dark");
+  }, [dark]);
+
   return (
-    <nav className="nav">
-      <h2>Rajkumar</h2>
-      <ul>
-        <li><a href="#home">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#skills">Skills</a></li>
-        <li><a href="#projects">Projects</a></li>
-        <li><a href="#contact">Contact</a></li>
-      </ul>
+    <nav className="fixed w-full bg-white dark:bg-slate-900 shadow z-50">
+      {/* <div className="max-w-6xl mx-auto flex justify-between items-center p-4">
+        <h1 className="text-xl font-bold text-indigo-600">Rajkumar</h1>
+
+        <ul className="hidden md:flex gap-6 font-medium">
+          <li><a href="#hero">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#skills">Skills</a></li>
+          <li><a href="#projects">Projects</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+
+        <button
+          onClick={() => setDark(!dark)}
+          className="bg-indigo-600 text-white px-3 py-1 rounded"
+        >
+          {dark ? "☀️" : "🌙"}
+        </button>
+      </div> */}
+
+       <h1 className="text-3xl font-bold underline primary">
+      Hello world!
+    </h1>
     </nav>
   );
 }
